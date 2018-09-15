4045039283927ddbc47ce34c1fc49beab8b6abba
jdkr <jdkr@users.noreply.github.com>
2018-09-15 13:11:46 +0200

    Update init.lua

--- textadept-dev-tools/init.lua
+++ textadept-dev-tools/init.lua
@@ -132,8 +132,6 @@ local init_project=function(project_filename)
         tools_project.configure_project() end
     local quick_open_project_call = function()
         io.quick_open(project.dir, project.filter) end
-    local quick_open_library_call = function()
-        tools_project.quick_open_library(project) end
     local find_in_project_call = function()
         local search_text=tools_project.get_search_text('Find in Project')
         if search_text~=nil and search_text~=''  then
@@ -152,6 +150,8 @@ local init_project=function(project_filename)
             tools_project.find_in_libs(project, search_text)
         end
     end
+    local quick_open_libs_call = function()
+        tools_project.quick_open_libs(project) end
     local close_lib_buffers_call = function()
         tools_project.close_lib_buffers(lib_filepaths)
     end
@@ -179,7 +179,7 @@ local init_project=function(project_filename)
     keys['ad']['c']['p'] = config_project_call
     keys['ad']['q']['o'] = {}
     keys['ad']['q']['o']['p'] = quick_open_project_call
-    keys['ad']['q']['o']['l'] = quick_open_library_call
+    keys['ad']['q']['o']['l'] = quick_open_libs_call
     keys['ad']['f'] = {}
     keys['ad']['f']['i'] = {}
     keys['ad']['f']['i']['p'] = find_in_project_call
@@ -210,13 +210,13 @@ local init_project=function(project_filename)
         {'New', tools_project.new_project},
         {'Configure', config_project_call},
         {'Quick open', quick_open_project_call},
-        {'Quick open in libraries', quick_open_library_call},
         SEPARATOR,
         {'Find in Project', find_in_project_call},
         {'Replace in Project', tools_project.replace_in_project},
         {'Find+Replace in Project', find_replace_in_project_call},
         SEPARATOR,
         {'Find in Libraries', find_in_libs_call},
+	{'Quick open in Libraries', quick_open_libs_call},
         {'Close Library Buffers', close_lib_buffers_call},
         SEPARATOR,
         {'Goto related Keyline', goto_related_keyline_call},
